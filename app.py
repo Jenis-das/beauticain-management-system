7229cb9f33e101f1bbfb5a8b27d341e7db56b838
jenis-das <108253491+Jenis-das@users.noreply.github.com>
2025-06-27 20:32:26 +0530

    added port and host for deployment

--- app.py
+++ app.py
@@ -481,4 +481,4 @@ def view_artist(artist_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(port="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0",debug=True, port=5000)
